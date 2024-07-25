74dc3c2949b216f9e4ea890dba162148ff7ef7ef
sht02048 <sht02048@gmail.com>
2024-07-25 18:07:17 +0900

    ✨ Mdx 파싱

---
+++ src/components/Features/PostBody/index.tsx
@@ -0,0 +1,21 @@
+import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import remarkBreaks from "remark-breaks";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import rehypePrettyCode from "rehype-pretty-code";
+
+export default function PostBody({ content }: { content: string }) {
+  return (
+    <div className="prose dark:prose-invert w-full max-w-3xl mx-auto">
+      <MDXRemote
+        source={content}
+        options={{
+          mdxOptions: {
+            remarkPlugins: [remarkGfm, remarkBreaks],
+            rehypePlugins: [rehypePrettyCode, rehypeSlug],
+          },
+        }}
+      />
+    </div>
+  );
+}
