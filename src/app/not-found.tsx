2c3f38d69e5fc7b856be3fee73adb2ab7e78eb8d
sht02048 <sht02048@gmail.com>
2024-07-27 14:26:08 +0900

    ✨ Notfound 컴포넌트 구현

---
+++ src/app/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import siteConfig from "../config/siteConfig";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full space-y-6">
+      <span className="text-9xl font-extrabold">404</span>
+      <p className="text-4xl font-medium">Oops! page is not found</p>
+      <p className="text-2xl">
+        But, you may find other interesting articles on my blog
+      </p>
+      <Link
+        href={siteConfig.path.home()}
+        className="bg-button px-2 py-1 rounded"
+      >
+        Back to Blog
+      </Link>
+    </div>
+  );
+}
