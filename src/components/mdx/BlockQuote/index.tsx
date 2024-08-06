7b2f184d4b68269523244a604ec28c7688d41b29
sht02048 <sht02048@gmail.com>
2024-08-06 14:28:51 +0900

    💄 Mdx 인용 컴포넌트 스타일 변경

--- src/components/mdx/BlockQuote/index.tsx
+++ src/components/mdx/BlockQuote/index.tsx
@@ -2,8 +2,8 @@ import { PropsWithChildren } from "react";
 
 export default function MdxBlockQuote(props: PropsWithChildren) {
   return (
-    <div className="font-medium mt-6 border-l-4 pl-5 border-secondary">
-      <strong>{props.children}</strong>
+    <div className="font-medium my-6 border-l-4 pl-5 border-secondary">
+      <p>{props.children}</p>
     </div>
   );
 }
