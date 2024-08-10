10c50ac0e9a25ac22fd8b6f1d7ab63f49c1ad6ff
sht02048 <sht02048@gmail.com>
2024-08-10 22:02:33 +0900

    ✨ 사이드 목차에 홈 및 위로가기 기능 추가

--- src/components/features/SideTableOfContents/index.tsx
+++ src/components/features/SideTableOfContents/index.tsx
@@ -2,10 +2,13 @@
 
 import clsx from "clsx";
 import Link from "next/link";
+import { IoHomeOutline } from "react-icons/io5";
+import { MdVerticalAlignTop } from "react-icons/md";
 
 import { Heading } from "@/src/config/types";
 import getFirstHighlight from "@/src/lib/getFirstHighlight";
 import useHeadingsObserver from "@/src/hooks/useHeadingObserver";
+import pathKeys from "@/src/config/pathKeys";
 
 export default function SideTableOfContents({
   headings,
@@ -20,10 +23,17 @@ export default function SideTableOfContents({
       highlightedHeadings: activeIdList,
     });
 
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
   return (
-    <div className="absolute -right-44 h-full top-0 hidden xl:block w-64">
-      <div className=" sticky top-1/3 border-l">
-        <div className="ml-4">
+    <div className="absolute -right-52 h-full top-0 hidden xl:block w-64">
+      <div className="sticky top-1/3 ">
+        <div className="pl-4 border-l">
           <div className="mb-2 font-bold">목차</div>
           <ol className="text-xs space-y-1">
             {headings.map((heading) => {
@@ -46,6 +56,14 @@ export default function SideTableOfContents({
               );
             })}
           </ol>
+        </div>
+        <div className="mt-6 text-xl flex space-x-4">
+          <Link href={pathKeys.home()}>
+            <IoHomeOutline />
+          </Link>
+          <button onClick={scrollToTop}>
+            <MdVerticalAlignTop />
+          </button>
         </div>
       </div>
     </div>
