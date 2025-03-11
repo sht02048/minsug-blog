7c99cd76cc5bd9f144aeb011d7a2c9f510f3f7ca
sht02048 <sht02048@gmail.com>
2025-03-11 22:27:47 +0900

    🚀 Speed insight 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "../config/css/tailwind.css";
 import siteConfig from "../config/siteConfig";
@@ -54,7 +53,6 @@ export default function RootLayout({
       <body>
         <MainWrapper>{children}</MainWrapper>
         <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
